89b3b8c825a6c179ec0fe15ef114803b65aa8dcc
Harsh17380 <106827228+Harsh17380@users.noreply.github.com>
2023-12-25 13:03:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/christmasgiftbyharsh/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.christmasgiftbyharsh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView Text;
+    private ImageView imageView;
+    private Button button;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Text = findViewById(R.id.Text);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You Won Lots of Chocolates , hugs and Kisses", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
